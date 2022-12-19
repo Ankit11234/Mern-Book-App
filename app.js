997b40d63cf72c15ccbdabc351d88be17870bfcf
Ankit11234 <ankittripathi13486@gmail.com>
2022-12-19 19:17:04 +0530

    final

--- app.js
+++ app.js
@@ -36,9 +36,10 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
 
 
-// if (process.env.NODE_ENV == "production") {
-//     app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
 // use Routes
 app.use('/api/books', books);
 
